89c523fdbaf8857843716426dfd1afeb992bb7ae
ZhangZhenghao <zhangzhenghao@hotmail.com>
2017-01-19 10:59:31 +0800

    Drop component bit

--- src/main/java/com/sine_x/regexp/state/CharClassState.java
+++ src/main/java/com/sine_x/regexp/state/CharClassState.java
@@ -4,25 +4,19 @@ import com.google.common.collect.RangeSet;
 
 public class CharClassState extends State {
 
-    private boolean complement = false;
     private RangeSet<Character> set;
 
     public CharClassState(RangeSet<Character> set) {
-        this(set, false);
-    }
-
-    public CharClassState(RangeSet<Character> set, boolean complement) {
         this.set = set;
-        this.complement = complement;
     }
 
     @Override
     public boolean match(char c) {
-        return complement ^ set.contains(c);
+        return set.contains(c);
     }
 
     @Override
     public String toString() {
-        return String.format("CharClassState(%s,%b)@%x", set, complement, hashCode());
+        return String.format("CharClassState(%s)@%x", set, hashCode());
     }
 }
